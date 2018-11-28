e660425027080ef859e472aafda7833bd8588fa7
shenles <lshen10@gmail.com>
2018-11-28 03:01:04 -0800

    Added map view on game detail pages

--- public/GameView.js
+++ public/GameView.js
@@ -30,6 +30,8 @@ gameReq.addEventListener("load", function(data){
 
 	document.getElementById("game_location").innerHTML = gameItem.location_name;
 
+        var zipcode = gameItem.location_zip; 
+
 	var userReq = new XMLHttpRequest();
 	userReq.addEventListener("load", function(data){
 		userItem = JSON.parse(this.responseText)[0];
@@ -57,6 +59,50 @@ gameReq.addEventListener("load", function(data){
 
 	usersGameReq.open('GET', "http://flip" + flipNumber + ".engr.oregonstate.edu:" + portNumber +"/users_in_game/" + gameItem.game_id, true);
 	usersGameReq.send();
+
+        var zipCoordReq = new XMLHttpRequest();
+        zipCoordReq.addEventListener("load", function(data){
+           zipCoordData = this.responseText;
+           var searchstring = zipcode + ","; 
+           var searchForZip = zipCoordData.indexOf(searchstring);
+            
+           if (searchForZip != -1) {
+             var firstCoordStart = searchForZip + 6;
+             var firstCoordEnd = firstCoordStart + 9;
+             var secondCoordStart = firstCoordEnd + 1;
+             var secondCoordEnd = secondCoordStart + 10;
+             var latString = zipCoordData.slice(firstCoordStart, firstCoordEnd);
+             var longString = zipCoordData.slice(secondCoordStart, secondCoordEnd);
+             var latnum = Number(latString);
+             var longnum = Number(longString);
+             var coordinates = [];
+             coordinates.push(latnum);
+             coordinates.push(longnum);
+           } else { 
+             var coordinates = []; 
+             coordinates.push(37.4419);
+             coordinates.push(-122.1430); 
+           } 
+           
+           var latlng = L.latLng(coordinates);
+           
+           var mymap = L.map('locationmap').setView(latlng, 13);
+
+           var mylayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+               attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+               maxZoom: 18,
+               id: 'mapbox.streets',
+               accessToken: 'pk.eyJ1Ijoic2hlbmxlcyIsImEiOiJjanAwbHF1aGIwNnR2M3ZvNjJicjB4YWZjIn0.djrqYZmfMzZKkrDrURti8w'
+              }).addTo(mymap);
+ 
+           var marker = L.marker(latlng).addTo(mymap);        
+    
+        });    
+       
+        zipCoordReq.open('GET', "https://gist.githubusercontent.com/erichurst/7882666/raw/5bdc46db47d9515269ab12ed6fb2850377fd869e/US%2520Zip%2520Codes%2520from%25202013%2520Government%2520Data", true);
+
+        zipCoordReq.send();
+        
 
 });
 
